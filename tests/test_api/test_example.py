dd443bf415396ebeb86ba64465b9255020c7eb14
Artem Krutalevich <art.krutalevich@gmail.com>
2023-07-25 14:25:53 +0400

    Stabilize pipeline

---
+++ tests/test_api/test_example.py
@@ -0,0 +1,16 @@
+from stests.lib.api.models.bclearer import Bclearer  # type: ignore
+
+
+def test_example():
+    foo = Bclearer()
+    foo.get_data().status_code
+
+
+def test_example1():
+    foo = Bclearer()
+    foo.get_data().status_code
+
+
+def test_example2():
+    foo = Bclearer()
+    foo.get_data().status_code
