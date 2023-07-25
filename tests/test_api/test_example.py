5c955e37b4f74021fe1237c752e05532a0fd484f
Artem Krutalevich <art.krutalevich@gmail.com>
2023-07-25 15:43:17 +0400

    No-capturing verbose output

--- tests/test_api/test_example.py
+++ tests/test_api/test_example.py
@@ -3,9 +3,11 @@ import requests
 
 def test_example():
     response = requests.get('http://localhost:5050/foo/bar')
+    print(response.text)
     assert response.status_code == 200
 
 
 def test_example2():
     response = requests.get('http://localhost:5050/foo/bar/baz')
+    print(response.text)
     assert response.status_code == 200
