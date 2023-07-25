8406fba619d0226cf0f1c2539b32faf35f996eb2
Artem Krutalevich <art.krutalevich@gmail.com>
2023-07-25 15:41:03 +0400

    Use the SUT in test

--- tests/test_api/test_example.py
+++ tests/test_api/test_example.py
@@ -1,7 +1,11 @@
 import requests
 
 
-
 def test_example():
     response = requests.get('http://localhost:5050/foo/bar')
+    assert response.status_code == 200
+
+
+def test_example2():
+    response = requests.get('http://localhost:5050/foo/bar/baz')
     assert response.status_code == 200
